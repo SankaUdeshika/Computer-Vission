20fa9880e015fcabe846aa31c888fdbeb5ca7867
Sanka Udeshika <sankaudeshika123@gmail.com>
2023-11-10 23:43:34 +0530

    Add 4.5.5 Jar File and test(Faild Test)

--- src/MainP/OpenWebCam.java
+++ src/MainP/OpenWebCam.java
@@ -4,10 +4,26 @@
  */
 package MainP;
 
-/**
- *
- * @author sanka
- */
-public class OpenWebCam {
-    
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+
+class OpenCVExample {
+
+    public static void main(String[] args) {
+        // Load the OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Read an image from file
+        Mat img = Imgcodecs.imread("your_image.jpg");
+
+        // Display the image
+        HighGui.imshow("Image", img);
+
+        // Wait for a key press and close the window
+        HighGui.waitKey(0);
+        HighGui.destroyAllWindows();
+    }
 }
