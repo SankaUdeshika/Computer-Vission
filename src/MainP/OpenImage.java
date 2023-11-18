8858eabb867e7442613d3478e01487669525f475
Sanka Udeshika <sankaudeshika123@gmail.com>
2023-11-18 19:06:27 +0530

    make a main method for openImage.jva

--- src/MainP/OpenImage.java
+++ src/MainP/OpenImage.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package MainP;
 
-/**
- *
- * @author sanka
- */
+
 public class OpenImage {
-    
+    public static void main(String[] args) {
+        System.out.println("OK Open Image");
+        System.out.println("OK Open Image");
+        System.out.println("OK Open Image");
+        System.out.println("OK Open Image");
+    }
+ 
 }
